e39a3a7c79b80595b81608e92f8f64260557f3db
Carolina Leon <leoncarolina35@gmail.com>
2025-04-30 15:01:30 +0200

    adding prettier

--- server/src/config/app.config.js
+++ server/src/config/app.config.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import database from './database.config.js';
 
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-database.then(() => {
-    console.log('Connected to database');
-}).catch((err) => {
-    console.log('Error connecting to database', err);
-});
-
-
+database
+	.then(() => {
+		console.log('Connected to database');
+	})
+	.catch((err) => {
+		console.log('Error connecting to database', err);
+	});
 
 export default app;
-
