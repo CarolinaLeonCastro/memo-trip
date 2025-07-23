f6af5ee29d2696ea0912e692c2a9d88ac05dcfed
Carolina Leon <leoncarolina35@gmail.com>
2025-07-23 21:50:23 +0200

    feat: Refactor App component structure and add Profile page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import ThemeProvider from './providers/ThemeProvider';
+import ThemeProvider from './providers/ThemeProvider';
 import { Box } from '@mui/material';
 import Layout from './components/Layouts/Layout';
 import Home from './pages/Home';
@@ -10,27 +10,31 @@ import EditJournal from './pages/EditJournal';
 import PlaceDetail from './pages/PlaceDetail';
 import { JournalProvider } from './context/JournalContext';
 import NewJournal from './pages/NewJournal';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <JournalProvider>
-      <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
-        <Router>
-          <Routes>
-            {/* me routes vont içi */}
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="journals" element={<Journals />} />
-              <Route path="journals/new" element={<NewJournal />} />
-              <Route path="journals/:id" element={<JournalDetail />} />
-              <Route path="journals/edit/:id" element={<EditJournal />} />
-              <Route path="place/:id" element={<PlaceDetail />} />
-              <Route path="profile" element={<div>Profil</div>} />
-            </Route>
-          </Routes>
-        </Router>
-      </Box>
-    </JournalProvider>
+    <ThemeProvider>
+      <JournalProvider>
+        <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
+          <Router>
+            <Routes>
+              {/* me routes vont içi */}
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="journals" element={<Journals />} />
+
+                <Route path="journals/new" element={<NewJournal />} />
+                <Route path="journals/:id" element={<JournalDetail />} />
+                <Route path="journals/edit/:id" element={<EditJournal />} />
+                <Route path="place/:id" element={<PlaceDetail />} />
+                <Route path="profile" element={<Profile />} />
+              </Route>
+            </Routes>
+          </Router>
+        </Box>
+      </JournalProvider>
+    </ThemeProvider>
   );
 }
 
